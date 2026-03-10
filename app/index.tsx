005fa078fdfd99cfbef0d07be2efb4ad68ecc16c
Anderson Sant Anna <anderson.santanna14@gmail.com>
2026-03-10 17:53:34 -0300

    feat: refactor initial layout

---
+++ app/index.tsx
@@ -0,0 +1,59 @@
+import SunIcon from "@/assets/icons/SunIcon";
+import {
+  WEATHER_GRADIENTS,
+  WeatherCondition,
+} from "@/constants/WeatherGradients";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TabOneScreen() {
+  const weatherCondition = WeatherCondition.STORMY; // This would typically come from your app's state or props
+  const gradient = WEATHER_GRADIENTS[weatherCondition];
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={gradient.colors}
+        locations={[0, 1]}
+        style={[StyleSheet.absoluteFill]}
+      />
+      <View
+        style={[styles.cloudEffect, { backgroundColor: gradient.cloudColor }]}
+      />
+      <Text style={styles.title}>Weather App</Text>
+      <Text style={styles.temp}>29°C</Text>
+      <SunIcon />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cloudEffect: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "75%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    opacity: 0.9,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#2E3A59",
+    zIndex: 1,
+  },
+  temp: {
+    fontSize: 72,
+    fontWeight: "bold",
+    color: "#4A6FA5",
+    marginTop: 20,
+    zIndex: 1,
+  },
+});
