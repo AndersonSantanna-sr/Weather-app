d0b5eed010df52df96b0c5bcf893afe33863e179
Anderson Sant Anna <anderson.santanna14@gmail.com>
2026-05-04 01:29:43 -0300

    test: add unit tests for useForecast React Query hook

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,23 @@ module.exports = {
       transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
       ],
-      testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+      testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/', 'useForecast.test.ts'],
+    },
+    {
+      displayName: 'hooks',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/features/**/__tests__/**/useForecast.test.ts'],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+        '^react-native$': 'react-native-web',
+      },
+      transform: {
+        '^.+\\.tsx?$': ['babel-jest', { presets: ['babel-preset-expo', '@babel/preset-typescript'] }],
+      },
+      transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@tanstack)',
+      ],
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.e2e.js'],
     },
   ],
 };
